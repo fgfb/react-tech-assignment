dfec1c60142b4c35c4934de0d25c9918df2ead76
JathinGanta <jathin.ganta@aize.io>
2024-10-24 20:59:08 +0100

    feat: Color-coded data points based on engineer ID

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,21 @@
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import {ACCESS_TOKEN, BICESTER_OFFICE_COORDINATES} from "../constants/constants";
+import {ACCESS_TOKEN, BICESTER_OFFICE_COORDINATES, STATUS_COLOR_MAP} from "../constants/constants";
 import {useEffect, useRef} from "react";
 import {DataType} from "../types";
 
 // Dummy access token used for this tech test only
 mapboxgl.accessToken = ACCESS_TOKEN;
+
+const customMarker = (engineerId: number) => {
+  const customMarker = document.createElement("div");
+  customMarker.style.borderRadius = '50%';
+  customMarker.style.height = '8px';
+  customMarker.style.width = '8px';
+  customMarker.style.background = STATUS_COLOR_MAP[engineerId] ?? '#3f3f3f';
+  customMarker.style.border = '1px solid white';
+  return customMarker;
+}
 
 function Map({ data }: { data: DataType }) {
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
@@ -20,8 +30,11 @@ function Map({ data }: { data: DataType }) {
       });
     }
 
-    data.forEach(({ coords }) => {
-      mapBoxRef.current && new mapboxgl.Marker().setLngLat(coords).addTo(mapBoxRef.current);
+    data.forEach(({ engineerId, coords }) => {
+      if (mapBoxRef.current) {
+        const marker = customMarker(engineerId);
+        new mapboxgl.Marker(marker).setLngLat(coords).addTo(mapBoxRef.current);
+      }
     })
 
   }, []);
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,12 @@
 export const ACCESS_TOKEN =
     'pk.eyJ1IjoiZXdvbmctZmlkbyIsImEiOiJjbTJkMmkyc2sxMzUwMmpxeGI2dDdndHN6In0.O2rAz61j2wq0fYrWU-2ULA';
 
-export const BICESTER_OFFICE_COORDINATES: [number, number] = [-1.15692, 51.92066]
+export const BICESTER_OFFICE_COORDINATES: [number, number] = [-1.15692, 51.92066];
+
+// TODO: Create a const for Engineer ID as only few specific numbers are being handled here
+export const STATUS_COLOR_MAP: Record<number, string> = {
+    [123]: '#f9af4c',
+    [124]: '#213a50',
+    [125]: '#e26161',
+    [126]: '#43b1cd',
+}
