df16c9f3f50d5d727dd847ea1e75046d379a654d
JathinGanta <jathin.ganta@aize.io>
2024-10-24 20:22:41 +0100

    feat: plot the data onto the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,32 @@
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import {ACCESS_TOKEN, BICESTER_OFFICE_COORDINATES} from "../constants/constants";
+import {useEffect, useRef} from "react";
+import {DataType} from "../types";
 
 // Dummy access token used for this tech test only
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZXdvbmctZmlkbyIsImEiOiJjbTJkMmkyc2sxMzUwMmpxeGI2dDdndHN6In0.O2rAz61j2wq0fYrWU-2ULA";
+mapboxgl.accessToken = ACCESS_TOKEN;
 
-function Map({ data }: { data: any[] }) {
-  // const coords = [-1.15692, 51.92066]; // Bicester office
-  return <div className="map" />;
+function Map({ data }: { data: DataType }) {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const mapBoxRef = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    if (mapContainerRef.current) {
+      mapBoxRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        center: BICESTER_OFFICE_COORDINATES,
+        zoom: 18
+      });
+    }
+
+    data.forEach(({ coords }) => {
+      mapBoxRef.current && new mapboxgl.Marker().setLngLat(coords).addTo(mapBoxRef.current);
+    })
+
+  }, []);
+
+  return <div className="map" ref={mapContainerRef} />;
 }
 
 export default Map;
---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN =
+    'pk.eyJ1IjoiZXdvbmctZmlkbyIsImEiOiJjbTJkMmkyc2sxMzUwMmpxeGI2dDdndHN6In0.O2rAz61j2wq0fYrWU-2ULA';
+
+export const BICESTER_OFFICE_COORDINATES: [number, number] = [-1.15692, 51.92066]
